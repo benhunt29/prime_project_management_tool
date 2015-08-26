0a2bd6ee0c3ab12c7f64dfb547083f3928380eed
benhunt29 <benhunt29@gmail.com>
2015-08-26 17:11:29 -0500

    started adding node/express functionality

--- server/app.js
+++ server/app.js
@@ -1,6 +1,21 @@
 express = require('express');
 employee = require('./employee');
-http = require('http');
+
+var app = express();
+
+app.get('/', function(req,res,next){
+    res.send("Name: " + employee.name + '\n');
+    res.send("Skills: " + employee.skills + '\n');
+    res.send("Scrum Points: " + employee.scrumPts + '\n');
+});
+
+app.post('/',function(req,res,next){
+    var assignedSkills = req.params;
+    //STOPPED HERE
+    res.send("Name: " + employee.name + '\n');
+    res.send("Skills: " + employee.skills + '\n');
+    res.send("Scrum Points: " + employee.scrumPts + '\n');
+})
 
 http.createServer(function (req, res) {
     res.writeHead(200);
