955cc7ad650bd626786517887b3943a3c8553bcf
benhunt29 <benhunt29@gmail.com>
2015-08-27 10:26:54 -0500

    added path

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-express = require('express');
-employee = require('./controllers/employee');
+var express = require('express');
+var employee = require('./controllers/employee');
+var path = require('path');
 
 var app = express();
 
 app.get('/', function(req,res,next){
-    res.send("Name: " + employee.name + '\n');
+    //res.send("Name: " + employee.name + '\n');
     res.send("Skills: " + employee.skills + '\n');
     res.send("Scrum Points: " + employee.scrumPts + '\n');
 });
@@ -17,9 +18,10 @@ app.post('/',function(req,res,next){
     res.send("Scrum Points: " + employee.scrumPts + '\n');
 });
 
+app.use(express.static(path.join(__dirname,'./public')));
 //app.use('/', index);
 //app.use('/users', users);
-app.use('/public/assets',express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use('./public/assets',express.static(__dirname + '/node_modules/jquery/dist/'));
 app.use('/public/assets',express.static(__dirname + '/node_modules/bootstrap/dist/css/'));
 app.use('/public/assets',express.static(__dirname + '/node_modules/adj-noun/'));
 app.use('/public/assets',express.static(__dirname + '/node_modules/chance/'));
